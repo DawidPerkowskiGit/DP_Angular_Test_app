ec5ee33364607f202445e806983516aa3986d4de
DawidPerkowskiGit <dtperkowski@gmail.com>
2023-07-10 15:43:21 +0200

    Testing resolution modifiers

---
+++ src/app/rooms/services/rooms.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { RoomList } from '../rooms';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoomsService {
+  constructor() {
+    console.log("Room Service constructor called");
+    
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 101,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0OxunpVLpH0iJ0TrgmW82zCCk2SqV0Vo6bQ&usqp=CAU.jpeg',
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+      rating: 3.7,
+    },
+    {
+      roomNumber: 102,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0OxunpVLpH0iJ0TrgmW82zCCk2SqV0Vo6bQ&usqp=CAU.jpeg',
+      checkinTime: new Date('13-Nov-2022'),
+      checkoutTime: new Date('14-Nov-2022'),
+      rating: 3.2,
+    },
+    {
+      roomNumber: 404,
+      roomType: 'VIP Room',
+      amenities:
+        'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen, Biliard Table',
+      price: 10000,
+      photos:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0OxunpVLpH0iJ0TrgmW82zCCk2SqV0Vo6bQ&usqp=CAU.jpeg',
+      checkinTime: new Date('9-Nov-2022'),
+      checkoutTime: new Date('10-Nov-2022'),
+      rating: 4.47789,
+    },
+  ];
+
+  getRooms() {
+    return this.roomList;
+  }
+}
